e5bef42f9b633f6b2d46c6e8cd2e59175ffe8ad5
magnoria <sidaumv@gmail.com>
2025-03-11 00:16:17 +0900

    집의 호스트 번호 다르니 오류시 확인할것 주소 및 번호 변경까지 완 , 한화면에서 수정 2개 하는방법 알아볼것

--- src/main/reactapp/src/hotels/Oper.jsx
+++ src/main/reactapp/src/hotels/Oper.jsx
@@ -1,12 +1,12 @@
 
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Operatae(props){
 
     //입력받은 데이터를 저장하는 변수
-    const[formData, setFomdata] = useState({address : '',hotel_number: '', intro : '', });
+    const[formData, setFomdata] = useState({ address : '',hotel_number: '', intro : ''});
 
     //입력받은 데이터를 렌더링
     const formDataChange = (e) => {
@@ -23,8 +23,51 @@ export default function Operatae(props){
             const response = await axios.post('http://localhost:8080/director' , formData)
             if(response.data == true){alert("등록성공");
                 setFomdata({address : '',hotel_number: '', intro : ''});
+                onFindAll();
             } else{alert('등록실패')};
         } catch (error) { console.log(error);}
+    }
+
+
+    const [boards, setBoards] = useState([]); //여러개의 방문록을 가지는 state 변수
+
+    const onFindAll = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/director');
+            setBoards(response.data);
+        } catch (error) {console.log(error);
+            
+        }
+
+    }
+
+    
+
+    //주소 소개 , 폰번호 변경 데이터
+    const [editFormData, setEditFormData] = useState({ hno : '' , hotel_number : '' , intro : ''});
+
+    const editChange = (e) => {
+        console.log(e.target);
+        console.log(e.target.name);
+        console.log(e.target.value);
+        setEditFormData({...editFormData, [e.target.name] : e.target.value});
+    }
+    
+    const onUpdate = async (hno) => {
+        try {
+                editFormData.hno = hno;
+            const response = await axios.put('http://localhost:8080/director', editFormData)
+            if(response.data == true){
+                alert("수정성공");
+                setEditFormData({hotel_number : '', intro : ''}); //초기화
+                onFindAll();
+            }else{
+                alert("수정실패");
+            }
+        } catch (error) {console.log(error);
+            
+        }
+
     }
 
     return(<>
@@ -36,18 +79,36 @@ export default function Operatae(props){
                     호텔 전화번호 : <input type="text" value={formData.hotel_number} name="hotel_number" onChange={formDataChange}/><br/>
                     소개 : <input type="text" value={formData.intro} name="intro" onChange={formDataChange}/><br/>
         
-                    <button type="button" onClick={onPost}>저장</button>
+                    <button type="button" onClick={onPost}>저장</button><br/><br/>
+
+                    수정 전화번호 : <input type="text" value={editFormData.hotel_number} name="hotel_number" onChange={editChange}/><br/>
+                    수정 소개 : <input type="text" value={editFormData.intro} name="intro" onChange={editChange}/><br/>
+                    
+
 
                 </form>
                 <table border={1}>
                     <thead>
-                        <tr><th>번호</th><th>주소</th><th>호텔전화번호</th><th>소개</th><th>운영상태</th><th>수정/삭제</th></tr>
-                        <tr><th>1</th><th>서울 강동구</th><th>010-3332-4243</th><th>우리호텔은~~~</th><th><select name="state" value={formData.state} onChange={formDataChange}>
+                        <tr><th>번호</th><th>주소</th><th>호텔전화번호</th><th>소개</th><th>운영상태</th><th>수정</th></tr>
+                        
+                    </thead>
+                    <tbody>
+                        {
+                            boards.map((board, index) => {
+                                return(<tr key={board.hno}><th>{board.hno}</th>
+                                <th>{board.address}</th>
+                                <th>{board.hotel_number}</th>
+                                <th>{board.intro}</th>
+                                <th><select name="state" value={formData.hno} onChange={formDataChange}>
                                     <option value='0'>운영중</option>
                                     <option value='1'>임시휴업</option>
                                     <option value='3'>폐점</option>
-                                </select></th><th><button type="button">수정</button><button type="button">삭제</button></th></tr>
-                    </thead>
+                                </select></th><th><button type="butoon" onClick={() => onUpdate(board.hno)}>수정</button></th></tr>)// 여기 부분 onUpdate() 를 그냥 넣으니 연속 실행됨
+                            })
+
+                        }
+
+                    </tbody>
                 </table>
 
             </div>
