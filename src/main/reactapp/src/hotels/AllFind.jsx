fa9a0478e6683134adb0e16d914c4487a53e61aa
magnoria <sidaumv@gmail.com>
2025-03-20 00:20:03 +0900

    FindAllcontroller 만들어서 각 정보 조회 확인 완, 지점관리 css 적용, img에 Link연결 함

---
+++ src/main/reactapp/src/hotels/AllFind.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar";
+
+
+
+
+export default function allFind(props){
+
+    
+
+
+    return(<>
+    <Sidebar />
+    allFind확인 완
+    </>)
+
+}
